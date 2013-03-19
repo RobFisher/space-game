448f25339aa946230b38e41e214bb6213457d799
Rob Fisher <robfisher@gmail.com>
2013-03-19 08:39:44 +0000

    Word generator makes syllables and other improvements.

--- word_generator.py
+++ word_generator.py
@@ -1,11 +1,11 @@
 __author__ = 'rob'
 
 
-# Table of which letters can follow a given letter at the start of a syllable.
-_start_letter_table = {
+# Table of which letters can follow a given letter
+_letter_table = {
     'a': 'abcdefghjklmnpqrstvwxyz',
     'b': 'abehiloruy',
-    'c': 'acehiklorsuwy',
+    'c': 'acehhhiklorsuwy',
     'd': 'adeijoruwy',
     'e': 'abcdefghijklmnopqrstuvwxyz',
     'f': 'aefhiloruwy',
@@ -18,11 +18,11 @@ _start_letter_table = {
     'm': 'aehimopuwy',
     'n': 'aehinouy',
     'o': 'abcdefghijklmnopqrstuvwxyz',
-    'p': 'aehinrstuwy',
+    'p': 'aehhinrstuwy',
     'q': 'aeiouuuuuuuuuuuuuuuuuuuuuy',
     'r': 'aehioruy',
-    's': 'aeilmnopqrtuwy',
-    't': 'aeiorsuyz',
+    's': 'accehhhhhilmnopqrtuwy',
+    't': 'aehhhhhiorsuyz',
     'u': 'abcdefghjklmnopqrstuvwxyz',
     'v': 'aeiouy',
     'w': 'aehioruy',
@@ -30,6 +30,8 @@ _start_letter_table = {
     'y': 'abcdefghijklmnopqrstuvwxyz',
     'z': 'aehiosuz'
 }
+
+_new_syllable_after_vowel = 'bcdfghjklmnpqrstvwxyz'
 
 _vowels = 'aaaeeeiioouy'
 _consonants = 'bbccdddffgghjklmmnnppqrrsssttvwxyz'
@@ -40,7 +42,8 @@ _double_consonants = {
     'ch': 'lr',
     'ph': 'lr',
     'sc': 'hlr',
-    'sh': 'cklmnprtw'
+    'sh': 'cklmnprtw',
+    'th': 'r'
 }
 
 
@@ -51,6 +54,18 @@ def _roll_d20(random):
 def _pick_letter(random, choices):
     index = random.randint(0, len(choices) - 1)
     return choices[index]
+
+
+_new_syllable_chance = [0, 0, 5, 10, 15, 16, 17, 18, 19]
+
+
+def _new_syllable(random, length, syllable):
+    for letter in syllable:
+        if letter in _vowels:
+            if length >= len(_new_syllable_chance) or _roll_d20(random) < _new_syllable_chance[length]:
+                return True
+            break
+    return False
 
 
 def make_word(random, min_length=3, max_length=20):
@@ -64,20 +79,30 @@ def make_word(random, min_length=3, max_length=20):
     """
     length = random.randint(min_length, max_length)
     result = _pick_letter(random, _letters)
+    if max_length > 1:
+        result += _pick_letter(random, _letter_table[result])
+    syllable_length = 2
     while len(result) < length:
-        if not result[-1:] in _vowels:
-            if _roll_d20(random) > 10:
+        if _new_syllable(random, syllable_length, result[0-syllable_length:]):
+            syllable_length = 0
+            if not result[-1:] in _vowels:
+                result += _pick_letter(random, _letter_table[result[-1:]])
+            else:
+                result += _pick_letter(random, _new_syllable_after_vowel)
+        elif not result[-1:] in _vowels:
+            if _roll_d20(random) > 15:
                 result += _pick_letter(random, _vowels)
-            elif len(result) > 1 and result[-2:-1] not in _vowels:
+            elif result[-2:-1] not in _vowels:
                 consonant_pair = result[-2:]
                 if consonant_pair in _double_consonants:
                     result += _pick_letter(random, _double_consonants[consonant_pair])
                 else:
                     result += _pick_letter(random, _vowels)
             else:
-                result += _pick_letter(random, _start_letter_table[result[-1:]])
+                result += _pick_letter(random, _letter_table[result[-1:]])
         else:
-            result += _pick_letter(random, _start_letter_table[result[-1:]])
+            result += _pick_letter(random, _letter_table[result[-1:]])
+        syllable_length += 1
     return result
 
 
@@ -85,4 +110,4 @@ if __name__ == '__main__':
     import random
     r = random.Random()
     for i in range(0, 200):
-        print make_word(r)
+        print make_word(r, 2, 8)
